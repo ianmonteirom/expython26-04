d2d180c2c7725633b6c361def9ee9b4df975cbc9
Ian <ianmonteiromoreira@gmail.com>
2024-04-26 09:15:13 -0300

    ex3

---
+++ ex3.py
@@ -0,0 +1,26 @@
+"""
+Preencha uma lista com 20 números inteiros aleatórios sorteados entre 1 e 50 e exiba:
+a. a lista com todos os itens armazenados.
+b. o somatório de todos os números contidos na lista.
+c. o maior número da lista.
+d. o menor número da lista.
+"""
+
+from random import randint
+
+randoms = []
+maior = 0
+menor = 51
+for r in range(20):
+    n = randint(1, 50)
+    randoms.append(n)
+    if n > maior:
+        maior = n
+    if n < menor:
+        menor = n
+
+soma = sum(randoms)
+print(f'Lista com todos os números aleatórios: {randoms}\n'
+      f'Somatório de todos os números: {soma}\n'
+      f'Maior número: {maior}\n'
+      f'Menor número: {menor}')
